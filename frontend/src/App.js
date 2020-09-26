b59d137f5d4b4b16942ad3c5bf2d7f5458f156b0
Abinash Sinha <asinha@DEV.com>
2020-09-26 17:48:40 +0530

    added site

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div id="root" class="container">     
+      <header class="navbar-fixed-top cbp-af-header">
+        <nav>
+          <ul>
+            <li class="logo"><a href="#"><i class="fa fa-file-code-o" aria-hidden="true"></i>
+
+            </a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </nav>
+        <button>Login</button>
       </header>
-    </div>
+  
+      <aside class="sidebar">
+        <h3>Recent Posts</h3>
+        <ul>
+            <li><a href="#">Post One</a></li>
+            <li><a href="#">Post Two</a></li>
+            <li><a href="#">Post Three</a></li>
+            <li><a href="#">Post Four</a></li>
+            <li><a href="#">Post Five</a></li>
+          </ul>
+      </aside>
+
+      <section class="main">
+        <img title="Sticky Notes" src="https://abbeyjfitzgerald.com/wp-content/uploads/2017/12/sticky-notes.jpg" alt="Sticky Notes">
+        </img>        
+        <h2>Sticky Positioning</h2>
+        <p>Sticky positioning is a combo of relative and fixed positioning. It’s useful for any UI element that you want to be “sticky” as user’s are scrolling. Users see it get “stuck” it when element gets to a specific distance from the the edge of the viewport. </p>
+        <p>We'll need a long page to demonstrate this. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </p>
+        <p>
+          Why do we use it?
+          It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+        </p>
+         <p> You'll see that the header stays in place when you scroll down.</p>
+      </section>
+
+    <footer>
+        <h3>Sticky Positioning Example</h3>
+        <p>Hope you enjoyed learning about how to make a sticky header!</p>
+    </footer>
+
+  </div>
+
+
+
   );
 }
 
