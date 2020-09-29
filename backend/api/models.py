e5838813f50d100e6e14b6cc2379d83d895272c2
Abinash Sinha <asinha@DEV.com>
2020-09-29 11:34:33 +0530

    Api

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 class NewBug(models.Model):
-    name = model.CharField('Name', max_length=240)
-    email = model.EmailField()
-    bugName = model.CharField("Bug")
-    bugDescription = model.CharField("Description")
-    project = model.CharField("Project Description")
-    date = model.DateField("Date", auto_now_add=True)
+    name = models.CharField('Name', max_length=240)
+    email = models.EmailField()
+    bugName = models.CharField("Bug", max_length=240)
+    bugDescription = models.CharField("Description", max_length=1000)
+    project = models.CharField("Project Description", max_length=240)
+    date = models.DateField("Date", auto_now_add=True)
     
     def __str__(self):
         return self.name
--- backend/api/views.py
+++ backend/api/views.py
@@ -6,7 +6,7 @@ from .models import NewBug
 from .serializers import *
 
 @api_view(['GET', 'POST'])
-def students_list(request):
+def bug_list(request):
     if request.method == 'GET':
         data = NewBug.objects.all()
 
@@ -22,20 +22,25 @@ def students_list(request):
             
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['PUT', 'DELETE'])
-def students_detail(request, pk):
-    try:
-        newbug = NewBug.objects.get(pk=pk)
-    except NewBug.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+@api_view(['GET', 'DELETE'])
+def bug_detail(request):
+    # try:
+    #     newbug = NewBug.objects.get(pk=pk)
+    # except NewBug.DoesNotExist:
+    #     return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'PUT':
-        serializer = NewBugSerializer(newbug, data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(status=status.HTTP_204_NO_CONTENT)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    # if request.method == 'PUT':
+    #     serializer = NewBugSerializer(newbug, data=request.data, context={'request': request})
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(status=status.HTTP_204_NO_CONTENT)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'DELETE':
         newbug.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+    elif request.method == 'GET':
+        data = NewBug.objects.all()
+        serializer = NewBugSerializer(data, context={'request': request}, many=True)
+        return Response(serializer.data)
